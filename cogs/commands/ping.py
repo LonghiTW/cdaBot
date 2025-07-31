a2b4bd863b790f1d5383fbae5a709b2b3df1f63b
LonghiTW <165005419+LonghiTW@users.noreply.github.com>
2025-07-31 15:51:23 +0800

    Update and rename ip.py to ping.py

--- cogs/commands/ping.py
+++ cogs/commands/ping.py
@@ -1,12 +1,12 @@
 from discord.ext import commands
 
-class IP(commands.Cog):
+class PING(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def ip(self, ctx):
-        await ctx.send("`bte.ntc.im`")
+    async def ping(self, ctx):
+        await ctx.send("pong!")
 
 async def setup(bot):
-    await bot.add_cog(IP(bot))
+    await bot.add_cog(PING(bot))
